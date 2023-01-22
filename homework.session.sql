7befac60c906cc22ca7290171f82904fb711af21
annlle <ua.lerus@gmail.com>
2023-01-22 23:52:07 +0200

    relations task

--- homework.session.sql
+++ homework.session.sql
@@ -1,20 +1,43 @@
-CREATE TABLE books(
+CREATE TABLE users(
     id serial PRIMARY KEY,
-    autor varchar(90) NOT NULL,
-    name_of_book varchar(255) NOT NULL,
-    publication_year int CONSTRAINT incorrect_year CHECK(publication_year <= 2023) NOT NULL,
-    publishing_house varchar(255) NOT NULL,
-    category_of_book varchar(100) NOT NULL,
-    synopsys varchar(1000),
-    num_of_copy int NOT NULL CONSTRAINT incorrect_number CHECK(num_of_copy >= 0),
-    status_of_book varchar(500) DEFAULT NULL
+    first_name varchar(255) NOT NULL,
+    last_name varchar(255) NOT NULL
 );
 
-ALTER TABLE books 
-ADD  CONSTRAINT must_be_not_empty CHECK (autor!='' AND name_of_book!=''),
-ADD CONSTRAINT uniqel_names UNIQUE(autor,name_of_book);
+CREATE TABLE messages(
+    chatID int REFERENCES chats(id),
+   text varchar(255) NOT NULL PRIMARY KEY,
+   time timestamp DEFAULT current_timestamp,
+   autor int REFERENCES users(id)
+);
 
-INSERT INTO books (autor,name_of_book,publication_year,publishing_house,category_of_book,synopsys,num_of_copy,status_of_book) VALUES
-('Franc Kafka', 'The Metamorphosis', 2017, 'Old Lion Publishing House', 'fantasy','The protagonist of the story, Gregor Samsa, a simple salesman, woke up in the morning and found that he had turned into a huge vile insect.', 100, 'The story really excites the hearts of young and not very readers who first get to know him. The work is filled with absurdity and surrealism, it is read very easily and interestingly.'),
-('William Shakespeare', 'The Metamorphosis', 2015,'Folio', '  tragic play', 'The main character sees the ghost of his father, who tells about his brother''s terrible crime. Prince Hamlet decides to get even with the murderer', 20, 'Here, the characters are so subtly and strongly created and shown that you can return to this work and re-read it until your death.'),
-('Oscar Wild', 'The Picture of Dorian Gray', 2020, 'Folio', 'novel', 'The dramatic, paradoxical, intriguing story of Dorian Gray, who became a victim of his desire for eternal life, remains attractive to both readers and filmmakers.', 150, 'The Picture of Dorian Gray is a very instructive and profound book that many should read. This is a story about a guy who accidentally traded his soul for eternal youth. It would seem, but is it possible?');
+CREATE TABLE chats(
+    id serial PRIMARY KEY,
+    owner int REFERENCES users(id)
+);
+
+CREATE TABLE chats_to_users(
+    chat_id int REFERENCES chats(id),
+    users_id int REFERENCES users(id),
+    message_text varchar(255) REFERENCES messages(text)
+);
+
+INSERT INTO chats(first_name, last_name) VALUES
+('John', 'Doe'),
+('Jane', 'Do'),
+('Sam', 'Smith');
+
+INSERT INTO chats(owner) VALUES
+(2),(3);
+
+INSERT INTO messages(chatID, text, autor) VALUES
+(1, 'Hi, how are you?', 2),
+(1, 'I''m fine, what about you?', 1),
+(3, 'What will you do tomorrow?',3);
+(3, 'I will work', 1);
+
+INSERT INTO chats_to_users(chat_id, users_id, message_text) VALUES
+(1,2, 'Hi, how are you?'),
+(1,2, 'I''m fine, what about you?'),
+(2,3, 'What will you do tomorrow?'),
+(2,1, 'I will work');
