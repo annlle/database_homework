2837a7804a99b7562169922b3c5769accd61c504
annlle <ua.lerus@gmail.com>
2023-01-29 22:36:28 +0200

    workers

--- homework.session.sql
+++ homework.session.sql
@@ -1,43 +1,70 @@
-CREATE TABLE users(
-    id serial PRIMARY KEY,
-    first_name varchar(255) NOT NULL,
-    last_name varchar(255) NOT NULL
+CREATE TABLE workers(
+    id SERIAL PRIMARY KEY,
+    name text CONSTRAINT name_null CHECK (name != '') NOT NULL,
+    salary int NOT NULL,
+    birthday date CONSTRAINT birthday_incorrect CHECK (birthday < current_date)
 );
 
-CREATE TABLE messages(
-    chatID int REFERENCES chats(id),
-   text varchar(255) NOT NULL PRIMARY KEY,
-   time timestamp DEFAULT current_timestamp,
-   autor int REFERENCES users(id)
-);
+--1
+INSERT INTO workers(name, salary, birthday) VALUES 
+('Oleg', 300, '1998-03-26');
 
-CREATE TABLE chats(
-    id serial PRIMARY KEY,
-    owner int REFERENCES users(id)
-);
+--2
+INSERT INTO workers(name, salary, birthday) VALUES
+('Jaroslava', 500, '1994-09-12');
 
-CREATE TABLE chats_to_users(
-    chat_id int REFERENCES chats(id),
-    users_id int REFERENCES users(id),
-    message_text varchar(255) REFERENCES messages(text)
-);
+--3
+INSERT INTO workers(name, salary, birthday) VALUES
+('Sasha', 1000, '1999-12-30'),
+('Masha', 200, '1995-11-01');
 
-INSERT INTO chats(first_name, last_name) VALUES
-('John', 'Doe'),
-('Jane', 'Do'),
-('Sam', 'Smith');
+--4
+UPDATE workers 
+SET salary = 500
+WHERE name = 'Oleg';
 
-INSERT INTO chats(owner) VALUES
-(2),(3);
+--5
+UPDATE workers
+SET salary = 400
+WHERE salary > 500;
 
-INSERT INTO messages(chatID, text, autor) VALUES
-(1, 'Hi, how are you?', 2),
-(1, 'I''m fine, what about you?', 1),
-(3, 'What will you do tomorrow?',3);
-(3, 'I will work', 1);
+--6
+SELECT * FROM workers
+WHERE salary > 400;
 
-INSERT INTO chats_to_users(chat_id, users_id, message_text) VALUES
-(1,2, 'Hi, how are you?'),
-(1,2, 'I''m fine, what about you?'),
-(2,3, 'What will you do tomorrow?'),
-(2,1, 'I will work');
+--7
+SELECT * FROM workers
+WHERE id = 4;
+
+--8
+SELECT extract('years' from age(birthday)) AS "вік", salary AS "заробітна платня" 
+FROM workers
+WHERE name = 'Zhenya';
+
+--9
+SELECT * FROM workers
+WHERE name = 'Petya';
+
+--10
+SELECT * FROM workers
+WHERE extract('years' from age(birthday)) = 27 OR salary > 800;
+
+--11
+SELECT name, extract('years' from age(birthday)) AS age FROM workers
+WHERE extract('years' from age(birthday)) BETWEEN 25 AND 28;
+
+--12
+SELECT id, name, birthday FROM workers
+WHERE extract('month' from birthday) = 9;
+
+--13
+DELETE FROM workers
+WHERE id = 4;
+
+--14
+DELETE FROM workers
+WHERE name = 'Oleg';
+
+--15
+DELETE FROM workers
+WHERE extract('years' from age(birthday)) > 30;
